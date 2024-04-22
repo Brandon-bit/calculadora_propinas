234110c9371c8ed930be90e349dc2cb0720777c2
Brandon Juarez <brandonsalinas792@gmail.com>
2024-04-22 16:42:37 -0600

    agregando hook personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import MenuItemComponent from "./components/MenuItem"
+import { menuItems } from "./data/db"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-8xl">Hola Mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="text-4xl font-black">Menú</h2>
+
+          <div className="space-y-3 mt-5">
+            {menuItems.map(item => (
+              <MenuItemComponent 
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
